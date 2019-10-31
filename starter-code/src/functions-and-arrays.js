2e99d619a32de8852ef80c31292a469c0984ed78
Daley James <20095@columbushs.com>
2019-10-31 12:44:59 -0400

    progress

--- starter-code/src/functions-and-arrays.js
+++ starter-code/src/functions-and-arrays.js
@@ -1,11 +1,42 @@
 // Iteration #1: Find the maximum
+function maxOfTwoNumbers(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  }
+  else {
+    return num2;
+  }
+
+};
+
+
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
+function findLongestWord (array){
+   if(array.length === 0){ 
+   return null;
+   }
+
+   let longestWord = array[0];
+   array.forEach(singleWord => {
+     if (longestWord.length < singleWord.length) {
+       longestWord = singleWord;
+
+     }
+   });
+   return longestWord;
+    }
+  
+  
+
+
 // Iteration #3: Calculate the sum
 
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+
+let sumArray = 
 
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
